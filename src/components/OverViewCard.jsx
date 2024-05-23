65fcf648c90bd4b0abb3c1ac9d447c6fc99f12c5
Jean Claude IRADUKUNDA <demo.claude15@gmail.com>
2024-05-23 11:41:56 +0200

    minor changes

--- src/components/OverViewCard.jsx
+++ src/components/OverViewCard.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const OverViewCard = ({total,text,icon}) => {
+const OverViewCard = ({ total, text, icon }) => {
   return (
-    <div className='bg-white p-5 w-[384px] rounded-md shadow-md h-[112px]'>
-        <div className='flex gap-10'>
-            <div className='w-[32px] h-[32px] rounded-full border flex justify-center items-center'>{icon}</div>
-            <div>
-                <p className='text-[22px] font-semibold text-[#030229cc]'>{total || 0}</p>
-                <p className='text-base text-[#030229d0] pt-2'>{text}</p>
-            </div>
-
+    <div className="bg-white p-5 w-[384px] rounded-md shadow-md h-[112px]">
+      <div className="flex gap-10">
+        <div className="w-[32px] h-[32px] rounded-full border flex justify-center items-center">
+          {icon}
         </div>
+        <div>
+          <p className="text-[22px] font-semibold text-[#030229cc]">
+            {total || 0}
+          </p>
+          <p className="text-base text-[#030229d0] pt-2">{text}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OverViewCard
+export default OverViewCard;
