3e4c2d546dd35cf8ac9a115f4623d3b0ac60e613
Jean Claude IRADUKUNDA <demo.claude15@gmail.com>
2024-05-30 09:21:51 +0200

    new changes

--- src/components/OverViewCard.jsx
+++ src/components/OverViewCard.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const OverViewCard = ({ total, text, icon }) => {
   return (
-<<<<<<< HEAD
-    <div className="bg-white p-5 w-[384px] rounded-md shadow-md h-[112px]">
+    <div className="bg-white px-5 pt-2 w-[384px] rounded-md shadow-md h-[112px]">
       <div className="flex gap-10">
         <div className="w-[32px] h-[32px] rounded-full border flex justify-center items-center">
           {icon}
@@ -13,16 +12,6 @@ const OverViewCard = ({ total, text, icon }) => {
             {total || 0}
           </p>
           <p className="text-base text-[#030229d0] pt-2">{text}</p>
-=======
-    <div className='bg-white px-5 pt-2 w-[384px] rounded-md shadow-md h-[112px]'>
-        <div className='flex gap-10'>
-            <div className='w-[32px] h-[32px] rounded-full border flex justify-center items-center'>{icon}</div>
-            <div>
-                <p className='text-[22px] font-semibold text-[#030229cc]'>{total || 0}</p>
-                <p className='text-base text-[#030229d0] pt-2'>{text}</p>
-            </div>
-
->>>>>>> c2a1edd6f5b87d03d3f5f71134b8a3c214728a84
         </div>
       </div>
     </div>
