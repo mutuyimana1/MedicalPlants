dc17f6af300c5a534387bc9b9ab9b42e334873fe
mutuyimana1 <amutuyimana64@gmail.com>
2024-05-31 19:47:40 +0200

    implement user pages

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = ({name,color,bgColor,border, hover,width,onClick}) => {
   return (
-    <div className={`text-${color} bg-${bgColor} hover:bg-[#025222] hover:text-white px-3 py-2 text-center cursor-pointer rounded-md w-${width} border border-${border}`} onClick={onClick}>
+    <button className={`text-${color} bg-${bgColor} w-full px-3 py-2 text-center cursor-pointer rounded-md w-${width} border border-${border}`} onClick={onClick}>
       {name}
-        </div>
+        </button>
   )
 }
 
